b8f2587e30616bf1abbb5ef2fa726d3e65944945
KavinPratama <kavinpratama11@gmail.com>
2026-06-17 08:41:57 +0700

    js ml lagi

--- A_PWEB_KAVIN/app/Providers/AppServiceProvider.php
+++ A_PWEB_KAVIN/app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // <-- INI YANG TADI KETINGGALAN CIK!
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        URL::forceScheme('https');
+        if (env('APP_ENV') !== 'local') {
+            URL::forceScheme('https');
+        }
     }
 }
